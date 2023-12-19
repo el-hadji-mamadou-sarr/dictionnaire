1e1bdcf677804fe9a5fc3367ae4758195ea525a1
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2023-12-19 16:37:41 +0100

    add new

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ type dictionnaire struct {
 
 func main() { 
 	
-	dict := dictionnaire{m: make(map[string]int)}
+	dict := new()
 	dict.add("one", 1)
 	dict.add("two", 2)
 	dict.get("one")
 	dict.list()
 	dict.remove("two")
+}
+
+func new() dictionnaire {
+	return dictionnaire{m: make(map[string]int)}
 }
 
 func (dict dictionnaire) add( key string, value int) {
