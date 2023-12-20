101d9f0b15aae62826e9d0b88016286e40591311
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2023-12-20 10:26:01 +0100

    refactor without using map

--- main.go
+++ main.go
@@ -6,21 +6,20 @@ import (
 	"io/ioutil"
 )
 
-type dictionnaire struct {
-	filePath string
-	entries  map[string]int
+type Entry struct {
+	Key   string `json:"key"`
+	Value int    `json:"value"`
 }
 
-func (dict *dictionnaire) checkError(){
-	if err := dict.saveToFile(); err != nil {
-		fmt.Println("Error saving to file:", err)
-	}
+type dictionnaire struct {
+	filePath string
 }
+
 
 func main() {
 	filePath := "dictionary.json"
 	dict := newDictionary(filePath)
-
+	
 	dict.add("one", 1)
 	dict.add("two", 2)
 	fmt.Println("Get 'one':", dict.get("one"))
@@ -28,42 +27,111 @@ func main() {
 	dict.remove("two")
 	dict.list()
 }
-
-func newDictionary(filePath string) dictionnaire {
-	return dictionnaire{
-		filePath: filePath,
-		entries:  make(map[string]int),
-	}
-}
-
-func (dict *dictionnaire) add(key string, value int) {
-	dict.entries[key] = value
-	dict.checkError()
-}
-
-func (dict *dictionnaire) get(key string) int {
-	return dict.entries[key]
-}
-
-func (dict *dictionnaire) remove(key string) {
-	delete(dict.entries, key)
-	dict.checkError()
-}
-
-func (dict *dictionnaire) list() {
-	for key, value := range dict.entries {
-		fmt.Println(key, value)
-	}
-}
-
-func (dict *dictionnaire) saveToFile() error {
-	data, err := json.MarshalIndent(dict.entries, "", "  ")
+func (dict *dictionnaire) saveToFile(entries []Entry) error {
+	jsonData, err := json.MarshalIndent(entries, "", "  ")
 	if err != nil {
 		return err
 	}
 
-	return ioutil.WriteFile(dict.filePath, data, 644)
+	err = ioutil.WriteFile(dict.filePath, jsonData, 644)
+	if err != nil {
+		return err
+	}
+
+	return nil	
+}
+
+func (dict *dictionnaire) loadFromFile() ([]Entry, error) {
+	jsonData, err := ioutil.ReadFile(dict.filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if the JSON data is empty
+	if len(jsonData) == 0 {
+		return nil, nil
+	}
+
+	var entries []Entry
+	err = json.Unmarshal(jsonData, &entries)
+	if err != nil {
+		return nil, err
+	}
+
+	return entries, nil
+}
 
 
+func (dict *dictionnaire) saving(entries []Entry) {
+	if err := dict.saveToFile(entries); err != nil {
+		fmt.Println("Error saving to file:", err)
+	}
+}
 
+func newDictionary(filePath string) dictionnaire {
+	return dictionnaire{
+		filePath: filePath,
+	}
+}
+
+func (dict *dictionnaire) add(key string, value int) {
+	entries, err := dict.loadFromFile()
+	if err != nil {
+		fmt.Println("Error loading from file:", err)
+		return
+	}
+
+	entry := Entry{Key: key, Value: value}
+	entries = append(entries, entry)
+	dict.saving(entries)
+}
+
+func (dict *dictionnaire) get(key string) int {
+	entries, err := dict.loadFromFile()
+	if err != nil {
+		fmt.Println("Error loading from file:", err)
+		return 0
+	}
+
+	for _, entry := range entries {
+		if entry.Key == key {
+			fmt.Println("Found:", entry)
+			return entry.Value
+		}
+	}
+
+	fmt.Println("Key not found:", key)
+	return 0
+}
+
+func (dict *dictionnaire) remove(key string) {
+	entries, err := dict.loadFromFile()
+	if err != nil {
+		fmt.Println("Error loading from file:", err)
+		return
+	}
+
+	for i, entry := range entries {
+		if entry.Key == key {
+			fmt.Println("Removing:", entry)
+			// Remove the entry from the slice
+			entries = append(entries[:i], entries[i+1:]...)
+			dict.saving(entries)
+			return
+		}
+	}
+
+	fmt.Println("Key not found:", key)
+}
+
+func (dict *dictionnaire) list() {
+	entries, err := dict.loadFromFile()
+	if err != nil {
+		fmt.Println("Error loading from file:", err)
+		return
+	}
+
+	for _, entry := range entries {
+		fmt.Println(entry)
+	}
 }
